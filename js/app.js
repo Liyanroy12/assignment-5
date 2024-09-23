79c6aedfc934a29c03452bc9b7c58bbe6b077ba9
Liyanroy12 <liyanray33@gmail.com>
2024-09-23 13:10:20 +0600

    block page added

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 const donateBtn = document.getElementById('donate-btn');
-donateBtn.addEventListener('click', function(){
+donateBtn.addEventListener('click', function () {
     const donateInput = parseFloat(document.getElementById('donate-input').value);
     const balanceDonate = parseFloat(document.getElementById('balance-donate').innerText);
-    // console.log(donateInput)
     const balanceAccount = parseFloat(document.getElementById('account-balance').innerText);
 
+    if (donateInput > balanceAccount) {
+        return alert('not enough money for donation');
+    }
+
+    if (isNaN(donateInput) || donateInput <= 0) {
+        return alert('invalid donation try again')
+    }
+    
     const newBalance = balanceAccount - donateInput;
     const newDonateBalance = balanceDonate + donateInput;
     document.getElementById('account-balance').innerText = newBalance.toFixed(2);
     document.getElementById('balance-donate').innerText = newDonateBalance.toFixed(2);
     document.getElementById('donate-input').value = '';
+})
+
+
+const loadedPage = document.getElementById('page-loaded');
+loadedPage.addEventListener('click', function(){
+    window.location.href='./block.html';
 })
